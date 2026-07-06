7eb00525c1e8b5e730b4d46b34881ec0c1c263ba
mscedeno-dev <mscedeno@sudamericano.edu.ec>
2026-07-06 01:13:54 -0500

    Remove build artifacts from version control

--- dist/src/users/dto/create-user.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { Role } from '@prisma/client';
-export declare class CreateUserDto {
-    name: string;
-    email: string;
-    password: string;
-    role?: Role;
-    phone?: string;
-    supervisorId?: string;
-}
